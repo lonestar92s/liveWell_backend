42a8d34677883f311e1f066258185ad81b392518
Andrew Aluko <aluko17@icloud.com>
2019-03-13 14:13:02 -0500

    walkscore api

--- controller/router.js
+++ controller/router.js
@@ -1,45 +1,49 @@
 const Zillow = require('node-zillow');
-const API_KEY = require('../api-key')
+const {API_KEY} = require('../api-key')
+const {WALKSCORE} = require('../api-key')
 const express = require('express')
 const router = express.Router()
 let app = express()
+const walkscore = require('walkscore-pk');
+ 
 
-
+  
 
 //Instantiate
 const zillow = new Zillow(API_KEY);
-const parameters = {
-  zpid: 48749425,
-};
+const ws = new walkscore();
 
-//get house 
-router.get('/', (req, res)=>{
-	zillow.get('GetZestimate', parameters)
-    .then(results=> {
-    	res.send(results)
-    })
-  	.catch(error => res.send(error))
-    // results here is an object { message: {}, request: {}, response: {}} 
-})    
-//Use form to accept "GetSearchResults" dynamically on submit
-router.get('/pic', (req, res)=> {
-  zillow.get('GetUpdatedPropertyDetails', parameters)
+// set the attributes needed
+ws.apiTransit = true;  // default true
+ws.apiBike = true;  // default true
+ws.apiLat = '47.6085';  // required
+ws.apiLon = '-122.3295';  // required
+ws.apiWsapikey = WALKSCORE;  // required
+
+// //test house 
+// router.get('/test', (req, res)=>{
+// 	zillow.get('GetDeepSearchResults', {address: '2114 Bigelow Ave', citystatezip: '98109'})
+//     .then(results=> {
+//     	console.log(results.response.results.result[0].address[0].longitude[0])
+//     })
+//   	.catch(error => res.send(error))
+//     // results here is an object { message: {}, request: {}, response: {}} 
+// })    
+
+router.get('/walkscore', (req, res)=>{
+   ws.getScores()
     .then(results=> {
       res.send(results)
     })
-    .catch(error => res.send(error))
-})
-
+    .catch(error => console.log(error))
+}) 
 //get deep search results
 router.post('/search', (req, res)=> {
  let {address, citystatezip} = req.body
   
-  console.log(req.body)
-  zillow.get('GetSearchResults', { address, citystatezip })
-    // .then(results => {
-    //   zillow.get('GetUpdatedPropertyDetails', results.response.results.result[0].zpid[0])
-    // })
+  zillow.get('GetDeepSearchResults', { address, citystatezip })
     .then(results=> {
+      console.log(results)
       res.send(results)
     })
     .catch(error => res.send(error))
