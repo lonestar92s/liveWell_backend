298d4b3485506fe55e49e2619f75204c459f9b37
Andrew Aluko <aluko17@icloud.com>
2019-03-13 16:49:47 -0500

    walkscore results

--- controller/router.js
+++ controller/router.js
@@ -16,8 +16,8 @@ const ws = new walkscore();
 // set the attributes needed
 ws.apiTransit = true;  // default true
 ws.apiBike = true;  // default true
-ws.apiLat = '47.6085';  // required
-ws.apiLon = '-122.3295';  // required
+ws.apiLat = '30.274545';  // required 
+ws.apiLon = '-97.740751';  // required
 ws.apiWsapikey = WALKSCORE;  // required
 
 // //test house 
@@ -39,16 +39,42 @@ router.get('/walkscore', (req, res)=>{
 }) 
 //get deep search results
 router.post('/search', (req, res)=> {
+  console.clear()
  let {address, citystatezip} = req.body
-  
-  zillow.get('GetDeepSearchResults', { address, citystatezip })
+ 
+      
+
+zillow.get('GetDeepSearchResults', { address, citystatezip })
+    
     .then(results=> {
       console.log(results)
-      res.send(results)
+      return results
+    })
+    .then (async results => {
+      let lng = results.response.results.result[0].address[0].longitude[0]
+      let lat = results.response.results.result[0].address[0].latitude[0]
+      ws.apiTransit = true;  // default true
+      ws.apiBike = true;  // default true
+      ws.apiLat = lat.toString();  // required 
+      ws.apiLon = lng.toString();  // required
+      ws.apiWsapikey = WALKSCORE;  // required
+      const walksults = await ws.getScores()
+        .then(walksults => {
+          console.log(walksults)
+       
+          return walksults
+        })
+        .catch(error => {
+          console.log(error)
+          return error
+        })
+        console.log(results)
+      res.send({ results, walksults}) 
     })
     .catch(error => res.send(error))
   }
 )
+
   	
 
 
